c62bf5d31f62db1e68dbf544857fd22d65c7e558
anonymous <78273014+noobforanonymous@users.noreply.github.com>
2021-02-08 23:23:48 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,43 @@
+red='\033[1;31m'
+clear='\033[0m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+clear
+echo " "
+echo " "
+echo '
+  __  ___ __  _   _       
+ /__)(_  / _ /_| /_| /| ) 
+/ (  /__(__)(  |(  |/ |/  
+                          
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         <===================wait===============>$clear"
+sleep 2.0
+clear
+echo -e "$red                          <===================we are checking===============>$clear"
+sleep 2.0
+clear
+echo -e "$red                         <===================any updates===============>$clear"
+sleep 2.0
+clear
+echo -e "$red                         <===================wait now it's===============>$clear"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$green               <=================== updating the DDattack please wait===============>$clear"
+sleep 2.0
+cd $HOME
+rm -rf getsourcecode
+git clone https://github.com/noobforanonymous/getsourcecode
+clear
+echo " "
+echo -e "$yellow              <=================== DDattack has been updated check it now===============>$clear"
+sleep 3.0
+echo " "
+cd $HOME
+clear
